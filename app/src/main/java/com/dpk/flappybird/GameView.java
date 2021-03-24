74fe1a4be7899058ff1a37377b577b752a6f6456
dipak <sahdeepak330@gmail.com>
2021-03-24 17:04:13 +0530

    fitting the background image to canvas size by getting display size

--- app/src/main/java/com/dpk/flappybird/GameView.java
+++ app/src/main/java/com/dpk/flappybird/GameView.java
@@ -1,17 +1,25 @@
 package com.dpk.flappybird;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Handler;
+import android.view.Display;
 import android.view.View;
 
 public class GameView extends View {
-Handler handler;
+     Rect rect;
+    Handler handler;
 Runnable runnable;
 final int milis=30;
 Bitmap background;
+Display display;
+Point point;
+ int dWidth,dHeight;
     public GameView(Context context) {
         super(context);
         handler=new Handler();
@@ -22,12 +30,18 @@ Bitmap background;
             }
         };
         background= BitmapFactory.decodeResource(getResources(),R.drawable.background);
+        display=((Activity)getContext()).getWindowManager().getDefaultDisplay();
+        point=new Point();
+        display.getSize(point);
+        dWidth=point.x;
+        dHeight=point.y;
+        rect=new Rect(0,0,dWidth,dHeight);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawBitmap(background,0,0,null);
+        canvas.drawBitmap(background,null,rect,null);
         handler.postDelayed(runnable,milis);
     }
 }
